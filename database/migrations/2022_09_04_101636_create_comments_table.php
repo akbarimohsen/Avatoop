48617c1fb8836e572f304720e2d3762bc25f1367
Daryazar <heydari.std00@gmail.com>
2022-09-14 12:16:53 +0430

    Modify api login register

--- database/migrations/2022_09_04_101636_create_comments_table.php
+++ database/migrations/2022_09_04_101636_create_comments_table.php
@@ -20,18 +20,9 @@ class CreateCommentsTable extends Migration
             $table->date('created_at');
             $table->integer('status')->default(0);
             $table->unsignedBigInteger('user_id');
-<<<<<<< HEAD:database/migrations/2022_04_20_123426_create_comments_table.php
-            $table->unsignedBigInteger('rss_id')->nullable();
-            $table->unsignedBigInteger('news_id')->nullable();
-            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('news_id')->references('id')->on('news')->onDelete('cascade')->onUpdate('cascade');
-            $table->foreign('rss_id')->references('id')->on('rsses')->onDelete('cascade')->onUpdate('cascade');
-
-=======
             $table->unsignedBigInteger('news_id');
             $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade')->onUpdate('cascade');
             $table->foreign('news_id')->references('id')->on('news')->onDelete('cascade')->onUpdate('cascade');
->>>>>>> f2322229f328c205d4101a6ae9e8d7edd29a7aad:database/migrations/2022_09_04_101636_create_comments_table.php
         });
     }
 
