be8e1014b1ba509ff2eaa96a4c54dd90fb59e367
kiavash nouri <kiavashnouri94@gmail.com>
2022-09-19 00:34:33 -0700

    AddTeam livewire back to the first state

--- app/Http/Livewire/Admin/TeamManagement/AddTeam.php
+++ app/Http/Livewire/Admin/TeamManagement/AddTeam.php
@@ -12,79 +12,79 @@ class AddTeam extends Component
 {
     use WithFileUploads;
 
-    public $title;
-    public $league_id;
-    public $description;
-    public $logo;
-    public Team $team;
+//    public $title;
+//    public $league_id;
+//    public $description;
+//    public $logo;
+//    public Team $team;
+//
+//    public function mount()
+//    {
+//        $this->team = new Team();
+//    }
+//
+//    protected $rules = [
+//        'title' => 'required|string',
+//        'league_id' => 'required',
+//        'description' => 'required|string',
+//        'logo' => 'required|image|mimes:png,jpg,jpeg'
+//    ];
+//
+//    public function handleAvatarUpload()
+//    {
+////        $year = now()->year;
+////        $month = now()->month;
+////        $day = now()->day;
+//        $dir = 'images/teams';
+//        $name = rand(100, 10000) . "_" . $this->logo->getClientOriginalName();
+//        $this->logo->storeAs($dir, $name);
+//        return $name;
+//    }
+//
+//    public function submit()
+//    {
+//        $this->validate();
+////        dd($this->league_id);
+//        $this->team->title = $this->title;
+//        $this->team->logo = $this->handleAvatarUpload();
+//        $this->team->league_id = $this->league_id;
+//        $this->team->description = $this->description;
+//        $this->team->save();
+//
+//        return redirect()->route('admin.teams');
 
-    public function mount()
-    {
-        $this->team = new Team();
-    }
+//    }
 
-    protected $rules = [
-        'title' => 'required|string',
-        'league_id' => 'required',
-        'description' => 'required|string',
-        'logo' => 'required|image|mimes:png,jpg,jpeg'
-    ];
+    public function submit(){
+        $data = $this->validate([
+            'title' => 'required|string',
+            'league_id' => 'required',
+            'description' => 'required|string',
+            'logo' => 'required|image|mimes:png,jpg,jpeg'
+        ]);
 
-    public function handleAvatarUpload()
-    {
-//        $year = now()->year;
-//        $month = now()->month;
-//        $day = now()->day;
-        $dir = 'images/teams';
-        $name = rand(100, 10000) . "_" . $this->logo->getClientOriginalName();
-        $this->logo->storeAs($dir, $name);
-        return $name;
-    }
+        $logo_name = time() . '.' . $this->logo->extension();
 
-    public function submit()
-    {
-        $this->validate();
-//        dd($this->league_id);
-        $this->team->title = $this->title;
-        $this->team->logo = $this->handleAvatarUpload();
-        $this->team->league_id = $this->league_id;
-        $this->team->description = $this->description;
-        $this->team->save();
+        //resize Image
+        $dest_path = public_path('/assets/images/teams');
+
+        $logo = Image::make($this->logo->path());
+
+        $logo->resize(370, 245 , function($constraint) {
+            $constraint->aspectRatio();
+        })->save($dest_path . '/' . $logo_name);
+
+        $data['logo'] = $logo_name;
+        $data['league_id'] = intval($data['league_id']);
+
+        $team = Team::create($data);
+
+        session()->flash('message', 'تیم شما با موفقیت ایجاد گردید.');
 
         return redirect()->route('admin.teams');
 
     }
 
-//    public function submit(){
-//        $data = $this->validate([
-//            'title' => 'required|string',
-//            'league_id' => 'required',
-//            'description' => 'required|string',
-//            'logo' => 'required|image|mimes:png,jpg,jpeg'
-//        ]);
-//
-//        $logo_name = time() . '.' . $this->logo->extension();
-//
-//        //resize Image
-//        $dest_path = public_path('/assets/images/teams');
-//
-//        $logo = Image::make($this->logo->path());
-//
-//        $logo->resize(370, 245 , function($constraint) {
-//            $constraint->aspectRatio();
-//        })->save($dest_path . '/' . $logo_name);
-//
-//        $data['logo'] = $logo_name;
-//        $data['league_id'] = intval($data['league_id']);
-//
-//        $team = Team::create($data);
-//
-//        session()->flash('message', 'تیم شما با موفقیت ایجاد گردید.');
-//
-//        return redirect()->route('admin.teams');
-//
-//    }
-//
     function delete($id)
     {
 
